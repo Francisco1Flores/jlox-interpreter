9c14bffc4385b1c0b32867263ebdc0c8c64223ea
Francisco Flores <francisco.giavay@gmail.com>
2024-07-17 12:34:19 -0300

    First commit

---
+++ src/main/java/lox/Scanner.java
@@ -0,0 +1,14 @@
+package lox;
+
+import java.util.List;
+
+public class Scanner {
+    private final String source;
+    public Scanner(String source) {
+        this.source = source;
+    }
+
+    public List<Token> scanTokens() {
+
+    }
+}
